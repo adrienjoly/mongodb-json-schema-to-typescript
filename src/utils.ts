f44fb1234d87d234eb5d38ddd7508ab523812dfc
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-09-08 17:50:49 +0200

    try to replace [a-z] regexps by extended unicode matchers which include accentuated characters

--- src/utils.ts
+++ src/utils.ts
@@ -159,15 +159,15 @@ export function toSafeString(string: string) {
     // remove accents, umlauts, ... by their basic latin letters
     deburr(string)
       // replace chars which are not valid for typescript identifiers with whitespace
-      .replace(/(^\s*[^a-zA-Z_$])|([^a-zA-Z_$\d])/g, ' ')
+      .replace(/(^\s*[^\p{Letter}_$])|([^\p{Letter}_$\d])/gi, ' ')
       // uppercase leading underscores followed by lowercase
-      .replace(/^_[a-z]/g, match => match.toUpperCase())
+      .replace(/^_\p{Lowercase_Letter}/g, match => match.toUpperCase())
       // remove non-leading underscores followed by lowercase (convert snake_case)
-      .replace(/_[a-z]/g, match => match.substr(1, match.length).toUpperCase())
+      .replace(/_\p{Lowercase_Letter}/g, match => match.substr(1, match.length).toUpperCase())
       // uppercase letters after digits, dollars
-      .replace(/([\d$]+[a-zA-Z])/g, match => match.toUpperCase())
+      .replace(/([\d$]+\p{Letter})/g, match => match.toUpperCase())
       // uppercase first letter after whitespace
-      .replace(/\s+([a-zA-Z])/g, match => trim(match.toUpperCase()))
+      .replace(/\s+(\p{Letter})/g, match => trim(match.toUpperCase()))
       // remove remaining whitespace
       .replace(/\s/g, '')
   )
